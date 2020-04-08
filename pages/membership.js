73a3125af76377c4ae93b1f5846dd98b336212f4
unknown <Dell@CIGRELITDL001.tv5.com.ph>
2020-04-08 16:07:24 +0800

    modified memberships style

--- pages/membership.js
+++ pages/membership.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Jumbotron, Button, Row, Col, Image, Container, CardGroup, Card, Modal } from 'react-bootstrap'
+import { Jumbotron, Button, Row, Col, Image, Container, CardGroup, Card, Modal, CardDeck } from 'react-bootstrap'
 import Link from 'next/link';
 import Router from 'next/router'
 import cookie from 'js-cookie'
@@ -11,6 +11,9 @@ import VideoMP4 from '../public/home/video.mp4';
 import VideoWEBM from '../public/home/video.webm';
 import JumboImage from '../public/home/jumbo.jpg';
 import SagisagImage from '../public/home/sagisag.jpg';
+import Pope from '../public/Image/pope.png';
+import Apec from '../public/Image/apec.png';
+import Potus from '../public/Image/potus.png';
 
 const jumboStyle = {
   background: `url(${JumboImage}) rgba(0, 0, 0, 0.4)`,
@@ -70,14 +73,14 @@ const Membership = () => {
    			<Container>
    			<Row className="show-grid">
 	    		<Col lg={6} md={6} sm={12} xs={12} className="mx-0 px-0 mb-4">
-	            	<div className="d-flex flex-column align-items-center justify-content-center" style={{border: "1px solid lightgray", borderRadius: "2%",minHeight: "50vh"}}>
+	            	<div className="d-flex flex-column align-items-center justify-content-center planBox1">
 	                	<h1 className="pBoxTitle">Individual Plan 1 Year</h1>
 	                	<h2>&#8369;<span className="font-weight-bolder">2,000</span><span> /yr</span></h2>
 	                	<Button onClick={handleIndiPlan} className="btn btn-success px-3 mt-3" style={{height: "50px", width: "150px", borderRadius: "100px"}}>Enroll Now</Button>
 	              	</div>
 	            </Col>
 	    		<Col lg={6} md={6} sm={12} xs={12} className="mx-0 px-0 mb-4">
-	            	<div className="d-flex flex-column align-items-center justify-content-center" style={{border: "1px solid lightgray", borderRadius: "2%",minHeight: "50vh"}}>
+	            	<div className="d-flex flex-column align-items-center justify-content-center planBox2">
 	                	<h1 className="pBoxTitle">Individual Plan 2 Years</h1>
 	                	<h2>&#8369;<span className="font-weight-bolder">3,000</span><span> /2yrs</span></h2>
 	              		<Button onClick={handleIndiPlan} className="btn btn-success px-3 mt-3" style={{height: "50px", width: "150px", borderRadius: "100px"}}>Enroll Now</Button>
@@ -99,7 +102,7 @@ const Membership = () => {
    			<Container>
    			<Row className="show-grid">
 	    		<Col lg={6} md={6} sm={12} xs={12} className="mx-0 px-0 mb-4">
-	            	<div className="d-flex flex-column align-items-center justify-content-center" style={{border: "1px solid lightgray", borderRadius: "2%",minHeight: "50vh"}}>
+	            	<div className="d-flex flex-column align-items-center justify-content-center planBox3">
 	                	<h1 className="pBoxTitle">Group Plan 1 Year</h1>
 	                	<h2>&#8369;<span className="font-weight-bolder">10,000</span><span> /yr</span></h2>
 	                	<small>Additional P1,700 per person for one year.</small>
@@ -107,7 +110,7 @@ const Membership = () => {
 	              	</div>
 	            </Col>
 	    		<Col lg={6} md={6} sm={12} xs={12} className="mx-0 px-0 mb-4">
-	            	<div className="d-flex flex-column align-items-center justify-content-center" style={{border: "1px solid lightgray", borderRadius: "2%",minHeight: "50vh"}}>
+	            	<div className="d-flex flex-column align-items-center justify-content-center planBox4">
 	                	<h1 className="pBoxTitle">Group Plan 2 Years</h1>
 	                	<h2>&#8369;<span className="font-weight-bolder">17,000</span><span> /2yrs</span></h2>
 	                	<small>Additional P2,850 per person for two years.</small>
@@ -121,11 +124,45 @@ const Membership = () => {
     	</Jumbotron>
     	<Jumbotron className="py-5 my-0 text-center d-flex align-items-center justify-content-center" style={{minHeight: "50vh"}}>
       		<div>
+				    <Row className="align-items-center">
+	    				<Col lg={4} md={4} sm={4} xs={4} className="mx-0 px-0 mb-4 text-right">
+					    	<img src={Potus} className="icon" />
+					    </Col>
+					    <Col lg={4} md={4} sm={4} xs={4} className="mx-0 px-0 mb-4">
+						    <img src={Pope} className="icon" />
+					    </Col>
+					    <Col lg={4} md={4} sm={4} xs={4} className="mx-0 px-0 mb-4 text-left">
+						    <img src={Apec} className="icon" />
+					    </Col>
+				    </Row>
+
 		        <h1 className="jumbo-title">You're in good company</h1>
 		        <h5 className="jumbo-subtitle">Join our satisfied customers locally and globally, and get secured by award winning service.</h5>
       		</div>
     </Jumbotron>
     </Container>
+    <style jsx>{`
+    	.icon{
+    		opacity: .3;
+    	}
+    	.icon:hover{
+    		opacity: 1;
+    	}
+    	.planBox1, .planBox2, .planBox3, .planBox4{
+    		border: 1px solid lightgray; 
+    		border-radius: 2%;
+    		min-height: 50vh;
+    		transition: transform .2s;
+    		
+    	}
+    	.planBox1:hover, .planBox2:hover, .planBox3:hover, .planBox4:hover{
+    		background-color: #dc3545;
+    		color: #FFF;
+    		transform: scale(1.1); 
+    		overflow: hidden;
+    	}
+    	
+    `}</style>
 		</Default>
 	)
 }
